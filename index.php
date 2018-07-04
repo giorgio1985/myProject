76a00219967a247b9b0cd4b4ba712f0b35882cbe
giorgio1985 <g.adonoo@yahoo.it>
2018-07-04 19:52:53 +0200

    development of app

--- index.php
+++ index.php
@@ -1,17 +1,15 @@
 <?php
 
-$username = $_POST['username'];
-$password = $_POST['password'];
-$email = $_POST['email'];
-$phone = $_POST['phone'];
+if (isset($_POST["username"]) && ($_POST["password"]) && ($_POST["email"]) && ($_POST["phone"])){
+$username = $_POST["username"];
+$password = $_POST["password"];
+$email = $_POST["email"];
+$phone = $_POST["phone"];
+	echo "mi e andata bene!!!";
 
-if (isset($_POST['username'])&&($_POST['password'])&&($_POST['email'])&&($_POST['phone']) ){
-	echo $_POST['username'];
 } else {
-	 //$rispo="ok ce lhai fatta ma adesso...";
-	echo "cazzoooo";
+	 
+	echo "mi e andatat male!!!";
 	
 }
-
-
 ?>
