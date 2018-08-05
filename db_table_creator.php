0991e755f244913cd910b7d9c57308ab46da1ef8
giorgio1985 <g.adonoo@yahoo.it>
2018-08-05 16:28:08 +0200

    last update 16.27 04-08-2018

--- db_table_creator.php
+++ db_table_creator.php
@@ -8,12 +8,13 @@ username VARCHAR(60) NOT NULL,
 email VARCHAR(60) NOT NULL,
 phone VARCHAR(12) NOT NULL,
 password VARCHAR(70) NOT NULL,
-reg_date TIMESTAMP
+reg_date TIMESTAMP,
+INDEX(id)
 ) ENGINE = InnoDB";
 if (mysqli_query($connect, $sql)) {
     echo "Table Login created successfully";
 } else {
-    echo "Error creating table: " . mysqli_error($connect);
+    echo "Error creating table login: " . mysqli_error($connect);
 }
 $sql2 = "CREATE TABLE IF NOT EXISTS videos (
 idVideo INT(6) AUTO_INCREMENT NOT NULL, 
@@ -21,13 +22,15 @@ fingerPrint2 VARCHAR(32) NOT NULL,
 nomeVideo VARCHAR(30) NOT NULL,
 descrizioneVideo VARCHAR(70) NOT NULL,
 genereVideo VARCHAR (30) NOT NULL,
+fileVideo VARCHAR(50) NOT NULL,
 timestampVideo TIMESTAMP,
-FOREIGN KEY (fingerPrint2) REFERENCES login (fingePrint1)
+INDEX(idVideo),
+FOREIGN KEY (fingerPrint2) REFERENCES login (fingerPrint1)
 )ENGINE =InnoDB";
 if (mysqli_query($connect, $sql2)) {
     echo "Table Videos created successfully";
 } else {
-    echo "Error creating table: " . mysqli_error($connect);
+    echo "  Error creating table videos: " . mysqli_error($connect);
 }
 mysqli_close($connect);
 ?>
--- video_custructor.php
+++ video_custructor.php
@@ -1,7 +1,7 @@
 <?php
 
-echo "custructor";
 session_start();
+//require './insert_video.php';
 require './db_connector.php';
 require './db_table_creator.php';
 $servername = "localhost";
@@ -16,12 +16,56 @@ if (!$connect) {
 }
 
 if (isset($_POST['load_name'], $_POST['load_description'],  $_POST['load_genere'], $_FILES['load_files']['name'])){
-	echo "is isset";
-}
+	echo " is isset";
+
 echo '<br>';
 echo $_POST['load_name']. '<br>';
 echo $_POST['load_description']. '<br>';
 echo $_POST['load_genere']. '<br>';
 echo $_FILES['load_files']['name']. '<br>';
 
-  ?>
+$_name_video = htmlspecialchars($_POST['load_name']);
+$_description_video = htmlspecialchars($_POST['load_description']);
+$_genere_video = htmlspecialchars($_POST['load_genere']);
+$_files_video = htmlspecialchars($_FILES['load_files']['name']);
+
+$_match_name_video = preg_match('/^[a-z0-9\s._-]{2,30}$/i', $_name_video);
+$_match_description_video = preg_match('/^[a-z0-9\s._-]{2,70}$/i', $_description_video);
+$_match_genere_video= preg_match('/^[a-z0-9\s.]{2,30}$/', $_genere_video);
+$_match_file_video = preg_match('/^[a-z0-9\s._-]{2,42}\.[a-z]{2,5}$/i', $_files_video);
+
+if (!($_match_name_video) || !($_match_description_video) || !($_match_genere_video) || !($_match_file_video)) {
+	if (!($_match_name_video)){
+	echo "error to match name video";
+}elseif ( !($_match_description_video) ) {
+	echo "error to match _description_video";
+}elseif (!($_match_genere_video)) {
+	echo "error to match genere video";
+}elseif (!($_match_file_video)) {
+	echo "error to match file video";
+}
+  mysqli_close($connect);
+ $back = <<<RETURN_BACK
+<script type="text/javascript">
+  window.location.href="./insert_video.php";
+</script>
+RETURN_BACK;
+
+echo $back; 
+}
+else
+echo "ok matching";
+echo $_SESSION["user"];
+$prova=$_SESSION["user"];
+
+$select_table_login = "SELECT fingerPrint1 FROM login WHERE username = '$prova'";
+$result_login = mysqli_query($connect, $select_table_login);
+if ($result_login) {
+  $sql= "INSERT INTO video (fingerPrint2, nomeVideo, descrizioneVideo, genereVideo) VALUES ('$select_table_login', '$_name_video', '$_description_video', '$_files_video')"; 
+  if (mysqli_query($connect, $sql)) {
+  	echo "ok insert to video table";
+  }  else  echo  mysqli_error($connect);
+
+}//mysqli_close($connect);
+}
+?>
